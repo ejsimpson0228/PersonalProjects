59240de13b32410058c6df586de4bb41c8b45c26
Eric Simpson <ejsimpson0228@gmail.com>
2018-04-21 07:32:41 -0400

    check in

---
+++ MyWorkoutLog/MyWorkoutLog.Tests/Tests/WorkoutTests.cs
@@ -0,0 +1,35 @@
+﻿using MyWorkoutLog.Data;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWorkoutLog.Tests.Tests
+{
+    [TestFixture]
+    public class WorkoutTests
+    {
+        [SetUp]
+        public void Init()
+        {
+            using (var cn = new SqlConnection(Settings.GetConnectionString()))
+            {
+                var cmd = new SqlCommand();
+                cmd.CommandText = "DbReset";
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Connection = cn;
+                cn.Open();
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+
+    }
+}
